98678245d8b0d730370c57b61ea13e9bfc8e8d7b
ketlinjardim <124809085+ketlinjardim@users.noreply.github.com>
2024-02-01 08:19:28 -0300

    Add files via upload

---
+++ aularevisão1/aula.ts
@@ -0,0 +1,9 @@
+
+let palavras : string[]
+ = ["keket","pedro","joao","no barquinho"]
+
+ palavras.splice(palavras.indexOf('joao'),0,"maça")
+
+//palavras.splice(2,0,"maça")
+
+console.log(palavras)
